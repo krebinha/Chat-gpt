2c71b74ef85d22398774fc7338c9c6642cc13e44
Cleber <59809499+krebinha@users.noreply.github.com>
2022-12-24 10:35:48 -0300

    Create Webscrapcurl.py
    Agora com curl

---
+++ Webscrapcurl.py
@@ -0,0 +1,28 @@
+import subprocess
+import xml.etree.ElementTree as ET
+
+# Executa o comando curl para fazer uma requisição HTTP GET para a página
+output = subprocess.run(['curl', '-s', url], capture_output=True).stdout
+
+# Verifica se a requisição foi bem-sucedida
+if output:
+  # Cria um objeto Beautiful Soup a partir da string HTML
+  soup = BeautifulSoup(output, 'html.parser')
+  
+  # Cria um elemento raiz para o documento XML
+  root = ET.Element('root')
+  
+  # Encontra todas as tags <a> que contêm um atributo 'href'
+  links = soup.find_all('a', href=True)
+  
+  # Adiciona os links encontrados como elementos filhos do elemento raiz
+  for link in links:
+    ET.SubElement(root, 'link').text = link['href']
+  
+  # Cria um objeto ElementTree a partir do elemento raiz
+  tree = ET.ElementTree(root)
+  
+  # Escreve o documento XML em um arquivo
+  tree.write('links.xml', encoding='utf-8', xml_declaration=True)
+else:
+  print("Erro ao fazer a requisição")
